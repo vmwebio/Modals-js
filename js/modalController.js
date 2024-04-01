0c3557be568dfc2dd797a9d2ebf1ad6014ee2978
vmwebio <vmwebio@gmail.com>
2024-04-01 20:01:51 +0300

    add scrollController

---
+++ js/modalController.js
@@ -0,0 +1,42 @@
+import {scrollController} from "./scrollController.js";
+
+export const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+    const buttonElems = document.querySelectorAll(btnOpen);
+    const modalElem = document.querySelector(modal);
+
+    modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+`;
+
+    const closeModal = event => {
+        const target = event.target;
+
+        if (target === modalElem ||
+            (btnClose && target.closest(btnClose)) ||
+            event.code === 'Escape'
+        ) {
+            modalElem.style.opacity = 0;
+
+            setTimeout(() => {
+                modalElem.style.visibility = 'hidden';
+                scrollController.enabledScroll(); // scroll on
+            }, time);
+            window.removeEventListener('keydown', closeModal);
+        }
+    }
+
+    const openModal = () => {
+        modalElem.style.visibility = 'visible';
+        modalElem.style.opacity = 1;
+        window.addEventListener('keydown', closeModal);
+        scrollController.disabledScroll(); // scroll off
+    }
+
+    buttonElems.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+    modalElem.addEventListener('click', closeModal);
+}
